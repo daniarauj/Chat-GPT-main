6c748b1316c102e62d7a256d472877ef8a49b0e6
daniarauj <134067241+daniarauj@users.noreply.github.com>
2023-08-03 16:33:27 -0300

    Initial commit

---
+++ funções.js
@@ -0,0 +1,48 @@
+function Area () {
+    let r=Number(prompt("Informe o valor do raio do circulo"))
+    let pi=Math.PI
+    let resultado=r**2*pi
+    alert("o resultado da área é "+resultado)
+    document.getElementById('a_ret').innerHTML=resultado
+}
+
+function Areab () {
+    let l1=Number(prompt("Informe o valor l1"));
+    let l2=Number(prompt("Informe o valor l2"));
+    let resultado=l1*l2
+    alert("O resultado da área é"+resultado+"m³")
+    document.getElementById('a_ret').innerHTML=resultado
+}
+
+function Areac () {
+    let l1=Number(prompt("Informe o valor l1"));
+    let l2=Number(prompt("Informe o valor l2"));
+    let resultado=l1*l2
+    alert("O resultado da área é"+resultado+"m³")
+    document.getElementById('a_ret').innerHTML=resultado
+}
+
+function Areac () {
+    let l1=Number(prompt("Informe o valor l1"));
+    let l2=Number(prompt("Informe o valor l2"));
+    let resultado=l1*l2
+    alert("O resultado da área é"+resultado+"m³")
+    document.getElementById('a_ret').innerHTML=resultado
+}
+
+function Aread () {
+    let l1=Number(prompt("Informe o valor l1"));
+    let l2=Number(prompt("Informe o valor l2"));
+    let l3=Number(prompt("Informe o valor l3"));
+    let resultado=l1*l2*l3
+    alert("O resultado da área é"+resultado+"m³")
+    document.getElementById('a_ret').innerHTML=resultado
+}
+
+function Areae () {
+    let l1=Number(prompt("Informe o valor l1"));
+    let l2=Number(prompt("Informe o valor l2"));
+    let resultado=l1*l2
+    alert("O resultado da área é"+resultado+"m³")
+    document.getElementById('a_ret').innerHTML=resultado
+}
